27ff92f7dad9b84bd394f14c1fbe6f43b4ab7895
Ilya <bkmz1840@gmail.com>
2021-05-09 23:38:50 +0500

    Minor changes

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.CharField(max_length=150, null=True, blank=True)
+    avatar = models.CharField(max_length=255, null=True, blank=True)
 
     def get_name_and_avatar(self):
         name = self.user.first_name + " " + self.user.last_name
