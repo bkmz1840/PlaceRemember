b700299f670278bd6fa96591eb10510ef4533e89
Ilya <bkmz1840@gmail.com>
2021-05-10 00:52:46 +0500

    add favicon restyle header

--- PlaceRemember/urls.py
+++ PlaceRemember/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("favicon.ico", RedirectView.as_view(url='favicon.ico', permanent=True)),
     path('', include('login.urls'), name="login"),
     path('', include("social_django.urls"), name="social"),
     path('myPlaceRemember/', include("myPlaceRemember.urls"), name="home_page"),
