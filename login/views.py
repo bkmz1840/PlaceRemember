137e956a0a77a9cc10a467a5afb1ed7313c3e65b
Ilya <bkmz1840@gmail.com>
2021-05-09 19:56:03 +0500

    Minor changes

--- login/views.py
+++ login/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render, redirect
 
 def index(request):
     if request.user.is_active:
-        return redirect("myPlaceRemember/")
+        return redirect("home")
     return render(request, 'login/index.html')
